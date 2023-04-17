c19f0c5e7b1f60e6ad16173a42bb44e6e55239bd
Victor Pedrosa <victor.pedrosa@mistplay.com>
2023-04-17 16:24:08 -0400

    Commit 5

--- app/src/main/java/com/mistplay/mergecommitpr/MapsActivity.kt
+++ app/src/main/java/com/mistplay/mergecommitpr/MapsActivity.kt
@@ -1,9 +1,8 @@
 package com.mistplay.mergecommitpr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -74,5 +73,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
      * iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum
      * qui dolorem eum fugiat quo voluptas nulla pariatur?
+     */
+
+    /**
+     * At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+     * deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non
+     * provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+     * fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta
+     * nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+     * omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis
+     * debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae
+     * non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
+     * maiores alias consequatur aut perferendis doloribus asperiores repellat.
      */
 }
